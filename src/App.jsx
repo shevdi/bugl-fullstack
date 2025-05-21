34619da3d2c52b94a7923a47d75011bde7702930
Your Name <you@example.com>
2025-05-21 20:10:31 +0300

    chore: gthub actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { Blog } from './Blog.jsx'
 const queryClient = new QueryClient()
 
 export function App() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <QueryClientProvider client={queryClient}>
       <Blog />
+      <button onClick={() => setIsOpen(!isOpen)}>{`${
+        isOpen ? 'Close' : 'Open'
+      } the devtools panel`}</button>
+      {isOpen && <ReactQueryDevtools onClose={() => setIsOpen(false)} />}
     </QueryClientProvider>
   )
 }
